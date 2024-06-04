461de0a868e4305431263e668b0bb9ef76a0d914
MaryamTaghavi <m.taghavi@megagroup.biz>
2024-06-04 12:39:05 +0330

    memoryCache

---
+++ PracticeMemoryCache/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace PracticeMemoryCache;
+
+public class HomeController : Controller
+{
+    //In-Memory-Caching / LocalCaching
+    private readonly IMemoryCache  _cache;
+    public HomeController(IMemoryCache  cache)
+    {
+        _cache = cache;
+    }
+    
+    public IActionResult Set1()
+    {
+        _cache.Set("CacheKey", "Hello World" , TimeSpan.FromDays(1));
+        return View();
+    }
+    
+    public IActionResult Get()
+    {
+        object data = _cache.Get("CacheKey");
+        return View(data);
+    }
+    
+    public IActionResult Set2()
+    {
+        DateTime data;
+        // Look for cache key.
+        if (!_cache.TryGetValue( "CacheKey" , out data))
+        {
+            // Key not in cache, so get data.
+            data= DateTime.Now;
+
+            // Save data in cache and set the relative expiration time to one day
+            _cache.Set( "CacheKey" , data, TimeSpan.FromDays(1));
+        }
+        return View(data);
+    }
+    
+    public IActionResult GetOrCreate()
+    {
+        var data = _cache.GetOrCreate( "CacheKey" , entry =>
+            entry.SlidingExpiration = TimeSpan.FromSeconds(3));
+        return View(data);
+    }
+
+    public IActionResult SetOptions()
+    {
+        //دور روش برای expiretime شدن کش وجود دارد:
+        //1- absolute expiration به این معنی است که یک زمان قطعی را برای منقضی شدن کش ها مشخص میکند به عبارتی میگوییم کش با کلید فلان در تاریخ و ساعت فلان حذف شود.
+        //2- sliding expiration یک بازه زمانی برای منقضی شدن کش ها مشخص میکنیم یعنی میگوییم بعد از گذشت فلان دقیقه از ایجاد کش ، آن را حذف کن.
+        
+        MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+        options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+        options.SlidingExpiration = TimeSpan.FromMinutes(1);
+        _cache.Set("CacheKey", "HelloWorld!", options );
+
+        return View();
+    }
+
+    public IActionResult LoadLevels()
+    {
+        //یکی از روش های مدیریت حافظه Ram در کش ها این است که برای حذف شدن کش ها از حافظه اولویت بندی هایی تعریف کنید. اولویت ها در چهار سطح قابل دسترسی است: Low = 0 / Normal = 1 / High = 2/ NeverRemove = 3
+        // این الویت بندی ها زمانی کاربرد خواهد داشت که حافظه اختصاصی Ram برای کش ها پر شده باشد و در این حالت سیستم کشینگ بصورت خودمختار کش های با الویت پایین را از حافظه حذف میکند و کش ها با الویت ببیشتر در حافظه باقی میماند.
+        var data = "Hello Worls" ;
+        MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+        
+        options.Priority = CacheItemPriority.High;
+        _cache.Set("CacheKey", data, options);
+
+        return View();
+    }
+}
---
+++ PracticeMemoryCache/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddMemoryCache();
+builder.Services.AddMvcCore();
+using IHost host = builder.Build();
+
+IMemoryCache cache =
+    host.Services.GetRequiredService<IMemoryCache>();
+
